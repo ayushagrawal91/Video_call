dbe9815af2ecfc309ecef9011bb43ab3435911fd
ayushagrawal91 <106581072+ayushagrawal91@users.noreply.github.com>
2024-07-14 17:22:03 +0530

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,34 +1,10 @@
-// import React, { ReactNode } from 'react'
-// import Navbar from '@/components/Navbar';
-// import Sidebar from '@/components/Sidebar'
-
-
-// const HomeLayout = ({children}: {children : ReactNode}) => {
-//   return (
-//     <main className= 'relative'>
-//         <Navbar />
-//         <div className= 'flex'>
-//             <Sidebar />
-
-//             <section className= "flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-//                 <div className= "w-full">
-//                   {children}  
-//                 </div>
-//             </section>
-//         </div>
-//         {children}
-//     </main>
-//   )
-// }
-
-// export default HomeLayout
 import React, { ReactNode } from 'react';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "VcMeet",   // Change App name
+  title: "QuickCall",   // Change App name
   description: "Video Calling App",
   icons: {
     icon: '/icons/logo.svg'  //Chnage Favicon
